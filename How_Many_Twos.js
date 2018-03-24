3c5a28d4154c33c2aaa7b92d10fdbf926f52b0a2
jaustindombrosky <austindombrosky@gmail.com>
2018-03-24 02:57:11 -0600

    hm2's

---
+++ How_Many_Twos.js
@@ -0,0 +1,24 @@
+//Write a function that returns the number of '2's in the factorization of a number.
+//For example,
+
+//twoCount(24)
+//should return 3, since the factorization of 24 is 2^3 x 3
+
+//twoCount(17280)
+//should return 7, since the factorization of 17280 is 2^7 x 5 x 3^3
+//The number passed to two_count (twoCount) will always be a positive integer greater than or equal to 1.
+// 7kyu
+
+function twoCount(n) {
+    var c = 0;
+    while(true){
+      if(n%2 === 0){
+        c++;
+        n/=2;
+      }
+      else{
+        break;
+      }
+    }
+    return c;
+  }
